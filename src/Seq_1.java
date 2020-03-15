63bdcbba14928f2eb0e0609ffed8c0d175b5496f
PratilipiAich <pratilipiaich@gmail.com>
2020-03-15 11:15:09 +0530

    Added Seq_1.java

--- src/Seq_1.java
+++ src/Seq_1.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Seq_1 {
     public static void main(String[] args) {
-        int n;
+        int num;
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the value of n:");
-        n = in.nextInt();
+        System.out.println("Enter the value of num:");
+        num = in.nextInt();
         System.out.println("Sequence 1: ");
-        for (int i = 2; i <= n; i += 2) {
+        for (int i = 2; i <= num; i += 2) {
             System.out.print(i * i + "\t");
         }
 
@@ -15,8 +15,8 @@ public class Seq_1 {
 }
 
 /*
-Enter the value of n:
+Enter the value of num:
 10
 Sequence 1:
-4	16	36	64	100
+4	16	36	64	100	
  */
