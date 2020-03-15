61bb8b24bff4213e8a8e0522bab823f9e97cd015
PratilipiAich <pratilipiaich@gmail.com>
2020-03-15 09:53:00 +0530

    Added Seq_3.java

---
+++ src/Seq_3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Seq_3 {
+    public static void main(String[] args) {
+        int n;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        n = in.nextInt();
+        System.out.println("Sequence 2:");
+        for (int i = 1; i <= n; i++) {
+            System.out.print((int) Math.pow(i, i) + "\t");
+        }
+    }
+}
+
+/*
+Enter the value of n:
+5
+Sequence 2:
+1	4	27	256	3125
+ */
