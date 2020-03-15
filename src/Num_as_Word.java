5dbdedd23b7e5d1fae147b3be02bdb63dc52a75e
PratilipiAich <pratilipiaich@gmail.com>
2020-03-15 11:13:04 +0530

    Added Num_as_Word.java

--- src/Num_as_Word.java
+++ src/Num_as_Word.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Num_as_Word {
@@ -5,55 +6,62 @@ public class Num_as_Word {
         int n, dig;
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the value of n:");
-        n = in.nextInt();
-        System.out.println(n + " in words is:");
-        int temp = n, rev = 0;
-        temp = n;
-        while (temp != 0) {
-            rev += temp % 10;
-            rev *= 10;
-            temp /= 10;
-        }
-        rev /= 10;
-        temp = rev;
-        while (temp != 0) {
-            dig = temp % 10;
-            switch (dig) {
-                case 0:
-                    System.out.print("Zero ");
-                    break;
-                case 1:
-                    System.out.print("One ");
-                    break;
-                case 2:
-                    System.out.print("Two ");
-                    break;
-                case 3:
-                    System.out.print("Three ");
-                    break;
-                case 4:
-                    System.out.print("Four ");
-                    break;
-                case 5:
-                    System.out.print("Five ");
-                    break;
-                case 6:
-                    System.out.print("Six ");
-                    break;
-                case 7:
-                    System.out.print("Seven ");
-                    break;
-                case 8:
-                    System.out.print("Eight ");
-                    break;
-                case 9:
-                    System.out.print("Nine ");
-                    break;
+        try {
+            n = in.nextInt();
+            System.out.println(n + " in words is:");
+            int temp = n, rev = 0;
+            temp = n;
+            while (temp != 0) {
+                rev += temp % 10;
+                rev *= 10;
+                temp /= 10;
             }
-            temp /= 10;
+            rev /= 10;
+            temp = rev;
+            while (temp != 0) {
+                dig = temp % 10;
+                switch (dig) {
+                    case 0:
+                        System.out.print("Zero ");
+                        break;
+                    case 1:
+                        System.out.print("One ");
+                        break;
+                    case 2:
+                        System.out.print("Two ");
+                        break;
+                    case 3:
+                        System.out.print("Three ");
+                        break;
+                    case 4:
+                        System.out.print("Four ");
+                        break;
+                    case 5:
+                        System.out.print("Five ");
+                        break;
+                    case 6:
+                        System.out.print("Six ");
+                        break;
+                    case 7:
+                        System.out.print("Seven ");
+                        break;
+                    case 8:
+                        System.out.print("Eight ");
+                        break;
+                    case 9:
+                        System.out.print("Nine ");
+                        break;
+                }
+                temp /= 10;
+            }
+        }
+        catch (InputMismatchException e) {
+            System.out.println("ERROR!\n" + e);
         }
     }
+
 }
+
 
 /*
 Enter the value of n:
