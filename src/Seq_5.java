1eecd8b7c209328867b0c5bbd60eed8d44f58e15
PratilipiAich <pratilipiaich@gmail.com>
2020-03-15 10:40:46 +0530

    Added Seq_5.java

--- src/Seq_5.java
+++ src/Seq_5.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
 
-public class Seq_3 {
+public class Seq_5 {
     public static void main(String[] args) {
         int n;
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the value of n:");
         n = in.nextInt();
-        System.out.println("Sequence 3:");
-        for (int i = 1; i <= n; i++) {
-            System.out.print((int) Math.pow(i, i) + "\t");
-        }
+        System.out.println("Sequence 5:");
+        for (int i = 1; i <= n; i++)
+            System.out.print(i * i + "\t");
     }
 }
 
 /*
 Enter the value of n:
 5
-Sequence 2:
-1	4	27	256	3125
+Sequence 5:
+1	4	9	16	25
  */
